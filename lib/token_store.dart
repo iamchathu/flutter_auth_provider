d395e0719428bb0ca8b270900c1746d61614e852
Chathu Vishwajith <4evercreations@gmail.com>
2021-06-05 12:00:34 +0530

    Add TokenStore interface

---
+++ lib/token_store.dart
@@ -0,0 +1,16 @@
+import 'dart:async';
+
+/// Token store interface. Implement this your own storage mechanism to store token and refreshToken.
+abstract class TokenStore {
+  /// Retrieve token is exists.
+  FutureOr<String?> getToken();
+
+  /// Retrieved return refreshToken exists.
+  FutureOr<String?> getRefreshToken();
+
+  /// Store [token] and [refreshToken]. [token] is mandatory.
+  void saveTokens({required String token, String refreshToken});
+
+  /// Clear token and refreshToken.
+  void clear();
+}
