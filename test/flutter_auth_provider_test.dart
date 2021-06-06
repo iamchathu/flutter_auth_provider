44b943b673c5fa7959ae6a091f1ab9f5e3a2b29d
Pasindu De Silva <pasindu@pasindu.local>
2021-06-06 23:11:11 +0800

    added simple test

--- test/flutter_auth_provider_test.dart
+++ test/flutter_auth_provider_test.dart
@@ -1,1 +1,26 @@
-void main() {}
+import 'package:flutter_auth_provider/auth_store.dart';
+import 'package:flutter_auth_provider/flutter_auth_provider.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+
+class FakeAuthStore<U> extends Fake implements AuthStore<U> {}
+
+void main() {
+  group(AuthProvider, () {
+    late AuthStore<String> authStore;
+    late AuthProvider<String> authProvider;
+
+    setUp(() {
+      authStore = FakeAuthStore<String>();
+      authProvider = AuthProvider<String>(authStore);
+    });
+
+    test('intially isLoggedIn is false', () {
+      expect(authProvider.isLoggedIn, false);
+    });
+
+    test('intially user is null', () {
+      expect(authProvider.user, isNull);
+    });
+  });
+}
